fc7b3a9af32a6ec51bb342f1cdad6795a3d03d58
Hasna Farhatul <hasna10222068@sttcipasung.ac.id>
2025-07-30 14:13:38 +0700

    login
    login

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+  window.addEventListener('DOMContentLoaded', function () {
+  //dashboard
   let isMenuOpen = false;
 
   function toggleMenu() {
@@ -6,3 +7,35 @@
     isMenuOpen = !isMenuOpen;
     menu.style.maxWidth = isMenuOpen ? "300px" : "0";
   }
+
+  //lupa password
+    const nextBtn = document.getElementById("nextBtn");
+  const resetForm = document.getElementById("resetForm");
+
+  if (nextBtn && resetForm) {
+    nextBtn.addEventListener("click", function () {
+      const emailInput = document.getElementById("email").value;
+
+      if (emailInput.trim() !== "") {
+        document.getElementById("passwordSection").style.display = "block";
+        document.getElementById("nextBtn").style.display = "none";
+        document.getElementById("submitBtn").style.display = "inline-block";
+      } else {
+        alert("Masukkan email terlebih dahulu.");
+      }
+    });
+
+    resetForm.addEventListener("submit", function (e) {
+      e.preventDefault();
+      const pass = document.getElementById("newPassword").value;
+      const confirm = document.getElementById("confirmPassword").value;
+
+      if (pass !== confirm) {
+        alert("Password tidak cocok!");
+        return;
+      }
+
+      alert("Password berhasil diubah! 🔒");
+    });
+  }
+});
